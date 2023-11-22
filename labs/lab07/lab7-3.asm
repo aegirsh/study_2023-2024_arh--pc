5be04cf75239d3f05b3a12bd0e2394363f8ea30f
Aleksandr Girshfeld <sashka.girshfeld@gmail.com>
2023-11-22 13:12:13 +0300

    feat(main): add files lab-7

---
+++ labs/lab07/lab7-3.asm
@@ -0,0 +1,45 @@
+%include 'in_out.asm'
+section .data
+msg2 db "Наибольшее число: ",0h
+A dd '44'
+B dd '74'
+C dd '17'
+
+section .bss
+max resb 10
+
+section .text
+
+global _start 
+_start:
+mov eax, B
+call atoi
+mov [B], eax
+
+mov ecx, [A]
+mov [max],ecx 
+
+cmp ecx, [C]
+jg check_B
+mov ecx,[C]
+mov [max], ecx
+
+
+check_B:
+mov eax,max
+call atoi
+mov [max],eax
+
+mov ecx,[max]
+cmp ecx,[B]
+jg fin
+mov ecx,[B]
+
+mov [max],ecx
+
+
+fin:
+mov eax, msg2
+call sprint
+mov eax,[max]
+call iprintLF
